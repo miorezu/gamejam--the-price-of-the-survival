ba151dc6572467d01643e099babddfa161073d8b
miorezu <m.miorezu@gmail.com>
2025-04-26 16:34:59 +0300

    player movement

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -5,24 +5,56 @@ public partial class Player : CharacterBody2D
 {
     [Export] public int Speed;
     [Export] public AnimatedSprite2D Sprite;
+    [Export] public AudioStreamPlayer Audio;
+    [Export] public CpuParticles2D FootStepsParticles;
 
-    public void GetInput()
+    private void Move()
     {
         Vector2 inputDirection = Input.GetVector("A", "D", "W", "S");
         Sprite.FlipH = inputDirection.X < 0;
-        Velocity = inputDirection * Speed;
+        Velocity = inputDirection;
+        Velocity = Velocity.Normalized() * Speed;
         if (inputDirection == Vector2.Zero)
         {
-            Sprite.Play("idle");
+            DisplayIdle();
         }
         else
         {
-            Sprite.Play("Run");
+            DisplayRun(inputDirection);
+            PlayMusic();
         }
     }
-    public override void _PhysicsProcess(double delta)
+    
+    public override void _Process(double delta)
     {
-        GetInput();
+        Move();
         MoveAndSlide();
+    }
+
+    private void DisplayIdle()
+    {
+        Sprite.Play("Idle");
+        FootStepsParticles.Emitting = false;
+        Sprite.FlipH = GetLocalMousePosition().X < 0;
+    }
+
+    private void DisplayRun(Vector2 inputDirection)
+    {
+        FootStepsParticles.Emitting = true;
+        FootStepsParticles.Direction = FootStepsParticles.Direction with { X = -1 };
+        if (inputDirection.X < 0)
+        {
+            FootStepsParticles.Direction = FootStepsParticles.Direction with { X = 1 };
+        }
+
+        Sprite.Play("Run");
+    }
+
+    private void PlayMusic()
+    {
+        if (!Audio.Playing)
+        {
+            Audio.Play();
+        }
     }
 }
