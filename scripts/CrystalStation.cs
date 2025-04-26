f1f4314d52d1319f9bd4be5af89260f6dcb73928
miorezu <m.miorezu@gmail.com>
2025-04-27 02:43:50 +0300

    CrystalStation

---
+++ scripts/CrystalStation.cs
@@ -0,0 +1,34 @@
+using Godot;
+using System;
+
+public partial class CrystalStation : StaticBody2D
+{
+    [Export] public TextureProgressBar EnergyBar;
+    [Export] public Timer Timer;
+    [Export] public AnimatedSprite2D AnimatedSprite;
+
+    public override void _EnterTree()
+    {
+        Timer.Timeout += TimerOnTimeout;
+    }
+
+    public override void _ExitTree()
+    {
+        Timer.Timeout -= TimerOnTimeout;
+    }
+
+    public override void _Process(double delta)
+    {
+        AnimatedSprite.Play("Idle");
+        GD.Print(EnergyBar.Value);
+        if (EnergyBar.Value == 0)
+        {
+            AnimatedSprite.Stop();
+        }
+    }
+
+    private void TimerOnTimeout()
+    {
+        EnergyBar.Value -= 10;
+    }
+}
